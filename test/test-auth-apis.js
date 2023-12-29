dd39acfe70b530a7343395df81c80475496fac35
Kunal Chandra <kunal.chandra1900@gmail.com>
2023-12-29 17:36:03 +0530

    Revert "added unit tests for all the auth and user apis"
    This reverts commit 2ea483195e170b304208ec35f20e57174c56a88c.

--- test/test-auth-apis.js
+++ test/test-auth-apis.js
@@ -1,9 +1,10 @@
 const mocha = require('mocha');
-const expect = require('chai').expect;
+// const chai = require('chai');
+const chai = import('chai');
+const expect = chai.expect;
 const assert = require('assert')
 const request = require('supertest');
 const app = require('../app');
-const User = require('../models/User');
 const baseURL = 'http://localhost:3000/api/v1/auth';
 // const request = Request(app);
 
@@ -15,19 +16,16 @@ describe('test user authentication APIs', function () {
             .post('/login')
             .send({ email: 'user2@gmail.com', password: '123456789' })
             .set('Accept', 'application/json')
-            .set('Content-Type', 'application/json')
             .end((err, res) => {
 
-                expect(res.status).to.be.equal(200)
-                expect(res.body.token).length.to.be.greaterThan(0)
-                expect(res.body.status).to.be.equal('success')
+                // expect(res.status).to.be.equal(200)
+                // expect(res.token).length.to.be.greaterThan(0)
+                // expect(res.status).to.be.equal('success')
 
-                // console.log(res.body.token);
-
-                // assert.equal(res.status, 200);
-                // assert.equal(res.body.status, 'success');
-                // assert.notEqual(res.body.token.length, 0);
-                // assert.notEqual(res.body.data.length, 0);
+                assert.equal(res.status, 200);
+                assert.equal(res.body.status, 'success');
+                assert.notEqual(res.body.token.length, 0);
+                assert.notEqual(res.body.data.length, 0);
 
                 if (err) {
                     console.log('Login API failing', err);
@@ -37,42 +35,31 @@ describe('test user authentication APIs', function () {
             })
     });
 
-    it('test user register api', function (done) {
-
-        const newUser = {
-            "name": "Test-user",
-            "username": "testuser_123",
-            "email": "test-user@gmail.com",
-            "password": "123456789",
-            "passwordConfirm": "123456789"
-        }
-
+    it.skip('test user register api', function (done) {
         request(baseURL)
             .post('/signup')
-            .send(newUser)
+            .send({
+                "name": "Test-user",
+                "username": "testuser_123",
+                "email": "test-user@gmail.com",
+                "password": "123456789",
+                "passwordConfirm": "123456789"
+            })
             .set('Accept', 'application/json')
-            .end(async (err, res) => {
+            .end((err, res) => {
 
-                console.log('Testing register api');
+                // expect(res.status).to.be.equal(200)
+                // expect(res.token).length.to.be.greaterThan(0)
+                // expect(res.status).to.be.equal('success')
 
-                expect(res.status).to.be.equal(200)
-                expect(res.body.token).length.to.not.equal(0)
-                expect(res.body.data).length.to.not.equal(0)
-                expect(res.body.success).to.be.equal(true)
-                expect(res.body.message).to.be.equal('User Registered successfully')
-
-
-                await User.findOneAndDelete({'email':'test-user@gmail.com'});
-
-                // assert.equal(res.status, 200);
-                // assert.equal(res.body.success, true);
-                // assert.equal(res.body.message.toLowerCase(), 'user registered successfully');
-                // assert.notEqual(res.body.token.length, 0);
-                // assert.notEqual(res.body.data.length, 0);
+                assert.equal(res.status, 200);
+                assert.equal(res.body.success, true);
+                assert.equal(res.body.message.toLowerCase(), 'user registered successfully');
+                assert.notEqual(res.body.token.length, 0);
+                assert.notEqual(res.body.data.length, 0);
 
                 if (err) {
                     console.log('Signup API failing', err);
-                    throw err
                 } else {
                     done();
                 }
--- test/test-user-apis.js
+++ test/test-user-apis.js
@@ -1,85 +1,33 @@
 const assert = require('assert');
 const mocha = require('mocha');
-const expect = require('chai').expect;
 const request = require('supertest');
-const User = require('../models/User');
 const baseURL = 'http://localhost:3000/api/v1'
-const app = require('../app');
+const app=require('../app');
 
 
-describe('test user APIs', function () {
+describe('test user APIs', async function () {
 
     let token;
 
-    this.beforeAll(function (done) {
-
-        const newUser = {
-            "name": "Test-user",
-            "username": "testuser_123",
-            "email": "test-user@gmail.com",
-            "password": "123456789",
-            "passwordConfirm": "123456789"
-        }
-
-        request(baseURL)
-            .post('/auth/signup')
-            .send(newUser)
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .end((err, res) => {
-
-                expect(res.status).to.be.equal(200)
-                expect(res.body.token).length.to.not.equal(0)
-                expect(res.body.data).length.to.not.equal(0)
-                expect(res.body.success).to.be.equal(true)
-                expect(res.body.message).to.be.equal('User Registered successfully')
-
-                // console.log(res.body.data);
-
-
-                // assert.equal(res.status, 200);
-                // assert.equal(res.body.success, true);
-                // assert.equal(res.body.message.toLowerCase(), 'user registered successfully');
-                // assert.notEqual(res.body.token.length, 0);
-                // assert.notEqual(res.body.data.length, 0);
-
-                if (err) {
-                    console.log('Signup API failing', err);
-                    throw err;
-                }
-
-                console.log('Test user created!');
-                done();
-            })
-    });
-
-    this.afterAll(async function () {
-        await User.findOneAndDelete({ 'email': 'test-user@gmail.com' });
-        console.log('Test user deleted!');
-    })
-
-    this.beforeEach(function (done) {
+    before(function () {
         request(baseURL)
             .post('/auth/login')
-            .send({ 'email': 'test-user@gmail.com', 'password': '123456789' })
+            .send({ 'email': 'test-user@gmail.com', password: '123456789' })
             .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
             .end(function (err, res) {
 
-                console.log('Logging test user!');
+                // console.log(res);
 
-                expect(res.body.status).to.equal('success')
-                expect(res.status).to.be.equal(200);
-                expect(res.body.token).length.to.not.equal(0);
-
+                assert.equal(res.body.status, 'success')
+                assert.equal(res.status, 200)
+                assert.notEqual(res.body.token.length, 0)
                 token = res.body.token;
 
                 // console.log(token);
-                done();
             })
-    })
+    });
 
-    it('test send friend request api', function (done) {
+    it.skip('test send friend request api', async function (done) {
         request(baseURL)
             .post('/users/sendrequest')
             .send({ "username": "user2_123" })
@@ -87,17 +35,17 @@ describe('test user APIs', function () {
             .set('Content-Type', 'application/json')
             .set('Cookie', `secret=${token}`)
             .end((err, res) => {
-                expect(res.status).to.be.equal(200);
-                expect(res.body.success).to.be.equal(true);
-                expect(res.body.message).to.equal('Friend Request Sent!');
+                assert.equal(res.status, 200);
+                assert.equal(res.body.success, true);
+                assert.equal(res.body.message, 'Friend Request Sent!');
 
                 if (err) {
                     console.log('Send friend request API failing!', err);
-                    throw err;
                 }
                 else {
                     done();
                 }
             });
     })
-});
+
+})
